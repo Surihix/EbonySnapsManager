66d97271127a2f54cf14acf0b5ec03ed801d4ff7
Surihix <neweramysterymailer@gmail.com>
2024-11-22 13:39:04 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MahApps.Metro.Controls;
+
+namespace EbonySnapsManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public static string StatusBarText { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            StatusBarText = "App launched!";
+        }
+    }
+}
